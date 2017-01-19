4b21fd3b0a6ec6e2aff8f26ca462fd29ccb122f4
Dickriven Chellemboyee <jchellem@gmail.com>
2017-01-19 23:27:04 +0400

    WIP

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        jshint: {
+            files: ['Gruntfile.js', '*.js'],
+            options: {
+                ignores:[
+                ],
+                globals: {
+                    jQuery: true
+                }
+            }
+        },
+        notify_hooks: {
+            options: {
+                enabled: true,
+                max_jshint_notifications: 5,
+                success: true,
+                duration: 3
+            }
+        },
+        bowercopy: {
+            options: {
+                clean: false
+            },
+            js: {
+                options: {
+                    destPrefix: 'public/vendor/js'
+                },
+                files: {
+                    'jquery.min.js': 'jquery/dist/jquery.min.js',
+                    'jquery.min.map': 'jquery/dist/jquery.min.map',
+                    'materialize.min.js': 'materialize/dist/js/materialize.min.js'
+                }
+            },
+            css: {
+                options: {
+                    destPrefix: 'public/vendor/css'
+                },
+                files: {
+                    'materialize.min.css': 'materialize/dist/css/materialize.min.css',
+                }
+            },
+            fonts:{
+                options: {
+                    destPrefix: 'public/vendor'
+                },
+                files: {
+                    'fonts': 'materialize/dist/fonts'
+                }
+            }
+        }
+    });
+
+    require('load-grunt-tasks')(grunt);
+    require('time-grunt')(grunt);
+
+    grunt.registerTask('default', [
+        'jshint',
+        'notify_hooks',
+        'bowercopy'
+    ]);
+};
