1097100e0f25b2e5ecc01749b8e7c0326229c60a
Dickriven Chellemboyee <jchellem@gmail.com>
2017-01-24 11:25:53 +0400

    pdf conversion

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fileUpload = require('express-fileupload');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -20,6 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileUpload());
 
 app.use('/', index);
 
